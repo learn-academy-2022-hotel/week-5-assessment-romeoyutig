9d104dc6ff6b8a4322d6015085ef567dbc324b2f
romeoyutig <romeoyutig2@outlook.com>
2023-01-17 08:12:57 -0800

    attempted assessment

--- interview-questions.md
+++ interview-questions.md
@@ -8,33 +8,33 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a class?
 
-Your answer:
+Your answer: Calling a super causes a class to inherit all of its properties, without having to initialize it again.
 
-Researched answer:
+Researched answer: Calling super causes the Superclass to pass down information to the subclass. This interaction is called inheritance, and is used by the "<" character. A subclass will inherit all of the data that the Superclass provides. The super must also be invoked in the initialization, before instance variables are declared. 
 
 2. What is a gem?
 
-Your answer:
+Your answer: A gem is a ruby dependency file. We use it mainly for testing. There are many ruby gems. 
 
-Researched answer:
+Researched answer: A gem is a snippet of code functionality that is managed by a packet manager. It allows you to download, and use ruby software packages on your system. They add large amounts of functionality to your software.
 
 3. What is a relational database? Are there other kinds of databases?
 
 Your answer:
 
-Researched answer:
+Researched answer: A relational database is the equivalent to a Google Docs sheet, with data provided in rows, and columns. Databases have tables, and they all can have relationships with each other. There are a large amount of databases, such as Object-oriented databases, and network databases, for example.
 
-4. What are primary keys? Why are they important?
+4. What are primary keys? Why are they important? 
 
-Your answer:
+Your answer: A primary key is the key that can be references in other databases. You can use this to connect databases to each other, cutting out on unecessary rows and cutting down on null values.
 
-Researched answer:
+Researched answer: A primary key is the key that can be references in other databases. You can use this to connect databases to each other, cutting out on unecessary rows and cutting down on null values. Rails will assign a primary key whenever a new table is created.
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: 
 
-Researched answer:
+Researched answer: The HTTP verbs are post, get, put, patch, and delete. The post verb will create data, the get verb will display data, the patch, and post verbs will update, and the delete verb will delete data. The accomplishments of all the CRUD actions using HTTP verbs will create a full-stack application.
 
 ## Looking Ahead: Terms for Next Week
 
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,6 +10,22 @@ letter_o = 'o'
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+# pseudo code :
+
+# create a method called letter_checker
+# input: array of words, and a single letter
+# expected output: an array of all the words containing that particular letter.
+# process: create a method called letter_checker with the parameters 'array', and 'string'
+# use the select method on the parameter array
+# print the select method to check if each array in the element includes the string provides in the parameter 
+# call the letter_checker method with the test variables provided included in the argument
+
+def letter_checker(array, string)
+  p array.select { |array| array.include?(string)}
+end
+letter_checker(beverages_array, letter_o)
+letter_checker(beverages_array, letter_t)
+
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
@@ -17,11 +33,38 @@ letter_t = 't'
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+# pseudo code:
+# create a method called updated_states
+# input: hash
+# output: one array with all the hash values at their own index and in alphabetical order.
+# process: use the methods .value to convert the hash into an array
+# chain the .flatten method to the code to combine the three arrays into one array
+# use the .sort method to alphabetize the combined array
+# p the parameter hash with all those methods chained
+# call on the updated_states method with the test variable as the parameter
+def updated_states(hash)
+  p hash.values.flatten.sort
+end
+updated_states(us_states)
+
+
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
+# pseudo code: create a class called Bike
+# input: class
+# output: string returning different instance variables data using string interpolation
+# process: initialize the class Bike
+# set the model, wheels, and current_speed as instance variables
+# set the default wheels value to 2, and the current_speed to 0
+# create a method to return a string utilizing the instance variable data through string interpolation
+# create a pedal_faster method
+# have the updated speed be set to the current_speed + the number provided in the argument
+# create a brake method
+# set a conditional returning 0 if the number provided in the argument is greater or equal to 0
+# all other scenarios return the current_speed subtracted by the number provided in the argument
 
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
@@ -30,3 +73,32 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+class Bike
+  def initialize(model)
+    @model = model
+    @wheels = 2
+    @current_speed = 0
+  end
+  def status_update
+    "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+  end
+  def pedal_faster(num)
+    @current_speed = @current_speed + (num)
+  end
+  def brake(num)
+    if num >= @current_speed
+      @current_speed = 0
+    else
+       @current_speed = @current_speed - num
+    end
+  end
+end
+
+my_bike = Bike.new('Trek')
+my_bike.pedal_faster(10)
+my_bike.pedal_faster(18)
+my_bike.brake(5)
+my_bike.brake(25)
+p my_bike.status_update
+
